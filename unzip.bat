be45a50540cffe71a2632093edb6f0faafc9870d
SashaArtamonov <sashaartamonovv@gmail.com>
2017-06-29 04:18:27 -0700

    modified ps file and removed cmd

--- unzip.bat
+++
@@ -1,6 +0,0 @@
-SET v_date=%1
-if [%v_date%] == [] SET v_date=%date:~10,4%%date:~4,2%%date:~7,2%
-cd "C:\TeamCity\buildAgent\work"
-::SET v_file="C:\TeamCity\buildAgent\work\e2dfe7db11bd71fd\students-3.zip"
-::unzip -o %v_file%
-"C:\Program Files\7-Zip\7z.exe" e "students-3.zip"
--- unzip.ps1
+++ unzip.ps1
@@ -6,4 +6,4 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-Unzip "C:\TeamCity\buildAgent\work\e2dfe7db11bd71fd\students-6.zip" "C:\TeamCity\buildAgent\work\63e415b4d300991e"
+Unzip "C:\TeamCity\buildAgent\work\e2dfe7db11bd71fd\*.zip" "C:\TeamCity\buildAgent\work\63e415b4d300991e"
