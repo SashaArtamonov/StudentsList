34213a3ab100e0ceb4c03b3c0d63bff63b74631e
SashaArtamonov <sashaartamonovv@gmail.com>
2017-06-29 06:59:26 -0700

    fixixif

--- deployment_test.cmd
+++
@@ -1,3 +0,0 @@
-msdeploy.exe -verb:sync -source:package=”C:\Users\Administrator\Desktop\msbuild\StudentsList\StudentsList\obj\Debug\Package\StudentsList.zip” -dest:iisApp=”students.local/subfolder01?,
-wmsvc=students.local,username=Administrator,password=vagrant,
-skipAppCreation=false -allowUntrusted=true
