fceee37f089216af70076344b1f8d68444bae4bf
SashaArtamonov <sashaartamonovv@gmail.com>
2017-07-11 01:55:22 -0700

    addad

---
+++ 1.cmd
@@ -0,0 +1,1 @@
+"C:\opencover.4.6.519\OpenCover.Console.exe" -target:"C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe" -targetargs:"/nologo C:\Users\Administrator\Desktop\StudentsList\Tests\bin\Debug\Tests.dll  /noshadow /framework:net-4.5" -excludebyattribute:"Kofile.Vanguard.Model.Tests" -register:user -output:"results.xml"
