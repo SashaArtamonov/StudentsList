706c2aa83985dace4780bf5aece704c245f71f5e
Sasha Artamonov <SashaArtamonov>
2017-11-20 04:24:29 -0800

    edited deploy

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,10 @@
 @echo off
 rem You should set next parameters as environment variables: USERNAME, PASSWORD
 
-set SERVER=ec2-52-32-247-141.us-west-2.compute.amazonaws.com
+set SERVER=localhost
 set SITE_NAME=students.local
-set USERNAME=Deploy
-set PASSWORD=123456Aa
+set USERNAME=Administrator
+set PASSWORD=vagrant
 
 set MSDEPLOY="C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
 
