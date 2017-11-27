b9fa849ac0f69a0b87f7e16dafa3da4f8d0d59a6
SashaArtamonov <Oleksandr_Artamonov@epam.com>
2017-11-27 13:37:15 -0800

    updatedde ploy

--- deploy.cmd
+++ deploy.cmd
@@ -29,7 +29,7 @@ IF "%SITE_NAME%"=="" (
 
 
 set _MsDeployAdditionalFlags=%_MsDeployAdditionalFlags% -setParam:'IIS Web Application Name'='%SITE_NAME%' -allowUntrusted
-call "%~dp0sample\obj\StudentsList.deploy.cmd " /Y /U:%USERNAME% /P:%PASSWORD% /M:%SERVER%
+call "%~dp0sample\obj\sample.deploy.cmd " /Y /U:%USERNAME% /P:%PASSWORD% /M:%SERVER%
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 Echo    ***** Deploy successful ***** 
